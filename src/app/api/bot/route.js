c6cf9861ec276c77b5474b626337f0dfe30a3828
ramo-dev <annuar.dev@gmail.com>
2024-07-28 00:27:25 +0300

    BOT update

--- src/app/api/bot/route.js
+++ src/app/api/bot/route.js
@@ -3,5 +3,5 @@ import { genAi } from "../genAi.js";
 export async function POST(request) {
   const { prompt } = await request.json();
   const resp = await genAi(prompt);
-  return Response.json({ "girrafeAi": resp });
+  return Response.json({ "AnnuarAi": resp });
 }
